203eecd61d80cafe846a074d56e4faf0ed4c57f1
Alexander Dikiy <vojtseh2014@gmail.com>
2025-05-26 22:48:54 +0200

    Add Move color highlight

---
+++ src/main/java/com/github/raumo0/languageplugindemo/MoveSyntaxHighlighterFactory.java
@@ -0,0 +1,17 @@
+package com.github.raumo0.languageplugindemo;
+
+import com.intellij.openapi.fileTypes.SyntaxHighlighter;
+import com.intellij.openapi.fileTypes.SyntaxHighlighterFactory;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VirtualFile;
+import org.jetbrains.annotations.NotNull;
+
+public class MoveSyntaxHighlighterFactory extends SyntaxHighlighterFactory {
+
+    @NotNull
+    @Override
+    public SyntaxHighlighter getSyntaxHighlighter(Project project, VirtualFile virtualFile) {
+        return new MoveSyntaxHighlighter();
+    }
+
+}
