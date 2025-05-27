ef446c959cc67494e583c4de66a65c21f86072f2
Alexander Dikiy <vojtseh2014@gmail.com>
2025-05-27 13:49:09 +0200

    Support Navigation Bar

---
+++ src/main/java/com/github/raumo0/languageplugindemo/MoveStructureAwareNavbar.java
@@ -0,0 +1,41 @@
+package com.github.raumo0.languageplugindemo;
+
+import com.intellij.icons.AllIcons;
+import com.intellij.ide.navigationToolbar.StructureAwareNavBarModelExtension;
+import com.intellij.lang.Language;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import javax.swing.*;
+
+final class MoveStructureAwareNavbar extends StructureAwareNavBarModelExtension {
+
+    @NotNull
+    @Override
+    protected Language getLanguage() {
+        return MoveLanguage.INSTANCE;
+    }
+
+    @Override
+    public @Nullable String getPresentableText(Object object) {
+        if (object instanceof MoveFile) {
+            return ((MoveFile) object).getName();
+        }
+        if (object instanceof MoveProperty) {
+            return ((MoveProperty) object).getName();
+        }
+
+        return null;
+    }
+
+    @Override
+    @Nullable
+    public Icon getIcon(Object object) {
+        if (object instanceof MoveProperty) {
+            return AllIcons.Nodes.Property;
+        }
+
+        return null;
+    }
+
+}
